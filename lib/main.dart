80309bc15435be744b8953d4db8cd41c709072d1
jetchatbotai <yusufeymenyagmur381@gmail.com>
2026-03-10 18:14:43 +0300

    Update main.dartconnect trinkExercises

--- lib/main.dart
+++ lib/main.dart
@@ -168,7 +168,8 @@ class WorkoutsScreen extends StatelessWidget { final Strings t; final EquipmentT
 
 const WorkoutsScreen({ super.key, required this.t, required this.equipment, required this.onEquipmentChanged, });
 
-@override Widget build(BuildContext context) { final filtered = equipment == EquipmentType.all ? FakeData.exercises : FakeData.exercises.where((e) => e.equipment == equipment).toList();
+@override Widget build(BuildContext context) { final filtered = equipment == EquipmentType.all ? trinkExercises
+:trinkExercises.where((e) => e.equipment == equipment).toList();
 
 return ListView(
   padding: const EdgeInsets.all(16),
